21adeabab8570d98a9329ed784154042795f6f18
Aleksey Belchenkov <u608110@gmail.com>
2018-07-21 00:31:12 +0300

    Vuetify, Vue Router

---
+++ resources/assets/js/components/AppHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-flex>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </v-flex>
+</template>
+
+<script>
+    import Toolbar from './Toolbar';
+    import AppFooter from './AppFooter';
+
+    export default {
+        components: {
+            Toolbar,
+            AppFooter
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Toolbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar id="toolbar" color="success" dark>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Laravel Vue Forum</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Forum</v-btn>
+            <v-btn flat>Ask Action</v-btn>
+            <v-btn flat>Category</v-btn>
+            <v-btn flat>Login</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Merienda:400,700');
+
+    #toolbar {
+        font-family: 'Merienda', cursive;
+        font-weight: bold;
+        text-shadow: 0 0 2px 5px #000;
+    }
+</style>
