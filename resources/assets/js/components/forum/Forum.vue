c632dc8c63737147ecc0889a738fe53551698223
Aleksey Belchenkov <u608110@gmail.com>
2018-09-26 06:55:34 +0300

    Forum Component

--- resources/assets/js/components/forum/Forum.vue
+++ resources/assets/js/components/forum/Forum.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>Forum</h1>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs8>
+        <question></question>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import question from './Question'
+
+export default {
+  components: {
+    question
+  }
+}
+</script>
---
+++ resources/assets/js/components/forum/Question.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="elevation-12" id="card">
+    <v-card-title primary-title>
+      <v-flex>
+        <h3 class="headline mb-0">Title of Question</h3>
+        <v-flex class="grey--text">Said 4 hour ago</v-flex>
+        </v-flex>
+    </v-card-title>
+
+   <v-card-text class="card-text">
+     Body
+   </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #card {
+    font-family: 'Titillium Web', sans-serif;
+    font-size: 1.3rem;
+  }
+
+  .card-text {
+    font-family: 'Dancing Script', cursive;
+    font-size: 1.4rem;
+  }
+</style>
