147bf04a66d5ef95100cece1133d5e0c190e32b7
Aleksey Belchenkov <u608110@gmail.com>
2018-10-10 07:03:37 +0300

    Show All Category

--- resources/assets/js/components/category/CreateCategory.vue
+++ resources/assets/js/components/category/CreateCategory.vue
@@ -13,6 +13,35 @@
             dark
           >Create</v-btn>
       </v-form>
+      <hr class="my-3">
+      <v-card class="elevation-12">
+          <v-toolbar color="green" dark dense>
+              <v-toolbar-title>Categories</v-toolbar-title>
+          </v-toolbar>
+          <v-list>
+              <v-flex v-for="category in categories" :key="category.id">
+                  <v-list-tile>
+                      <v-list-tile-action>
+                          <v-btn icon small>
+                              <v-icon color="orange">edit</v-icon>
+                          </v-btn>
+                      </v-list-tile-action>
+                      <v-list-tile-content>
+                          <v-list-tile-title>
+                              {{ category.name }}
+                          </v-list-tile-title>
+                      </v-list-tile-content>
+                      <v-list-tile-action>
+                          <v-btn icon small>
+                              <v-icon color="red">delete</v-icon>
+                          </v-btn>
+                      </v-list-tile-action>
+                  </v-list-tile>
+                  <v-divider></v-divider>
+              </v-flex>
+          </v-list>
+      </v-card>
+
   </v-container>
 </template>
 
@@ -22,8 +51,14 @@
             return {
                 form: {
                     name: ''
-                }
+                },
+                categories: {}
             }
+        },
+        created () {
+            axios.get('/api/category')
+                .then(res => this.categories = res.data.data)
+                .catch(err => console.error(err))
         },
         methods: {
             submit () {
